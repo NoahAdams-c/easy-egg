ec0a6d1d53ea1a5ec57b26df1715feda037822b5
chenchen <chenchen_9694@outlook.com>
2021-03-16 18:22:56 +0800

    本地路径获取完成，配置界面全部完成

--- server.js
+++ server.js
@@ -3,11 +3,12 @@
  * @Author: chenchen
  * @Date: 2021-03-12 14:17:03
  * @LastEditors: chenchen
- * @LastEditTime: 2021-03-16 15:09:15
+ * @LastEditTime: 2021-03-16 17:40:12
  */
 const express = require("express")
 const app = express()
 const path = require("path")
+const fs = require("fs")
 const body_parser = require("body-parser")
 const { port } = require("./config")
 
@@ -31,8 +32,24 @@ module.exports = {
 		app.use(express.static(path.join(__dirname, "views")))
 		// josn parser
 		app.use(body_parser.json())
-		app.use("/path", (req, res) => {
-			res.send(__dirname)
+
+		app.get("/files", (req, res) => {
+			const currentPath = req.query.path
+				? decodeURIComponent(req.query.path)
+				: process.cwd()
+			const dirents = fs.readdirSync(currentPath, { withFileTypes: true })
+			const files = dirents.map((dirent) => {
+				return {
+					name: dirent.name,
+					isDir: dirent.isDirectory()
+				}
+			})
+			res.send({
+				status: 0,
+				currentPath,
+				files,
+				isWin: process.platform === "win32"
+			})
 		})
 
 		// create server listen
